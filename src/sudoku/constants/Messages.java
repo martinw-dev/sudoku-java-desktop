6956105b0cf4f108c3225b5e8c6475a7bbc83452
martinw-dev <martinwalsh.dev@gmail.com>
2022-08-24 08:59:58 +0100

    State, Message & Row constants

---
+++ src/sudoku/constants/Messages.java
@@ -0,0 +1,7 @@
+package sudoku.constants;
+
+public class Messages {
+    public static final String GAME_COMPLETE = "Congratulations, you won! New Game?";
+    public static final String Error = "An error has occurred.";
+
+}
