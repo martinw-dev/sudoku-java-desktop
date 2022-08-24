02ca73da39f289f033c3673ecb6ca74926ad8f7c
martinw-dev <martinwalsh.dev@gmail.com>
2022-08-24 08:40:50 +0100

    grid state methods

--- src/sudoku/problemdomain/SudokuGame.java
+++ src/sudoku/problemdomain/SudokuGame.java
@@ -1,4 +1,25 @@
 package sudoku.problemdomain;
 
-public class SudokuGame {
+import java.io.Serializable;
+
+public class SudokuGame implements Serializable {
+    private final GameState gameState;
+    private final int[][] gridState;
+
+    public static final int GRID_BOUNDARY = 9;
+
+    public SudokuGame(GameState gamestate, int[][] gridState){
+        this.gameState = gamestate;
+        this.gridState = gridState;
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    // Making gridState immutable, stopping the Sudoku game object
+    // from being altered overtime
+    public int[][] getCopyOfGridState() {
+        return SudokuUtilities.copyToNewArray(gridState);
+    }
 }
