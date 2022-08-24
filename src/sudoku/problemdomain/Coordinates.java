0e17cd3dc9dc943436ecf95085021756687fa13a
martinw-dev <martinwalsh.dev@gmail.com>
2022-08-24 08:41:25 +0100

    equals & hashcode overrides

---
+++ src/sudoku/problemdomain/Coordinates.java
@@ -0,0 +1,34 @@
+package sudoku.problemdomain;
+
+import java.util.Objects;
+
+public class Coordinates {
+    private final int x;
+    private final int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Coordinates currentObj = (Coordinates) obj;
+        return x == currentObj.x && y == currentObj.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
